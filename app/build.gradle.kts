1bc136018c7051aaffb61ded9f5a5617e450b4cc
pflucian-pyt <pflucian@gmail.com>
2026-09-23 11:08:48 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         // pentru unitati mai vechi, daca vor aparea.
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.3"
+        versionCode = 4
+        versionName = "0.4"
     }
 
     /*
